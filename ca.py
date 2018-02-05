d7c7e9a7d96ec4570a133edf8600c512ece989d4
Mohammed b. Terry b. Jack <32687826+mohammedterry@users.noreply.github.com>
2018-02-05 07:54:04 +0000

    Update ca.py

--- ca.py
+++ ca.py
@@ -1,37 +1,55 @@
-def rule188(a,b,c):
-	if a == b != c:
-		return '0'
-	elif a == b == c == '1':
-		return '0'
-	return '1'
+def rule30(a,b,c):
+    if int(a) + int(b) + int(c) == 1:
+        return '1'
+    elif a != b == c == '1':
+        return '1'
+    return '0'
 
+def rule54(a,b,c):
+    if int(a) + int(b) + int(c) == 1:
+        return '1'
+    elif b != a == c == '1':
+        return '1'
+    return '0'
+    
 def rule110(a,b,c):
-	if a== b== c:
-		return '0'
-	elif a== '1' and b== c == '0':
-		return '0'
-	else:
-		return '1'
-		
+    if a== b== c:
+        return '0'
+    elif a != b == c == '1':
+        return '0'
+    return '1'
+
+def rule126(a,b,c):
+    if a == b == c:
+        return '0'
+    return '1'
+
+def rule188(a,b,c):
+    if a == b == c == '0':
+        return '0'
+    elif a == b != c:
+        return '0'
+    return '1'
+
 def display(row):
-	white = u'\u25A1'
-	black = u"\u25A0"
-	show = ''
-	for n in row:
-		if n == '1':
-			show += white
-		else:
-			show += black
-	print(show)
+    white = u'\u25A1'
+    black = u"\u25A0"
+    show = ''
+    for n in row:
+        if n == '1':
+            show += black
+        else:
+            show += white
+    print(show)
 
-def run(ic = '1'*45, t = 107):
-	row = ic
-	for _ in range(t):
-		first,last = row[0],row[-1]
-		nextrow = ''
-		for a,b,c in zip(row,  row[1:], row[2:]):
-			nextrow += rule110(a,b,c)
-		row = first + nextrow + last
-		display(row)
+def run(rule = rule110, ic = '0'*30 + '1' + '0'*30, t = 30):
+    row = ic
+    for _ in range(t):
+        display(row)
+        first,last = row[0],row[-1]
+        nextrow = ''
+        for a,b,c in zip(row,  row[1:], row[2:]):
+            nextrow += rule(a,b,c)
+        row = first + nextrow + last
 
-run()
+run(rule = rule188)
